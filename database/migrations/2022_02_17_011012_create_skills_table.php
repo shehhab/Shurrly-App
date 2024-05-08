3da7f8ce8306a0864a0e300dd888f5ac5d810630
shehhab <shehabahsraf308@gmail.com>
2024-05-08 13:16:28 +0300

    update_skill

--- database/migrations/2022_02_17_011012_create_skills_table.php
+++ database/migrations/2022_02_17_011012_create_skills_table.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Models\Skill;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -19,27 +18,9 @@ return new class extends Migration
             $table->foreignId('categories_id')->nullable()->references('id')->on('categories');
             $table->timestamps();
         });
-
-        $skillData = [
-            ['name' => 'Business', 'public' => true, 'categories_id' => 1, 'image_path' => 'Default\Category\Business.PNG'],
-            ['name' => 'Economics', 'public' => true, 'categories_id' => 1, 'image_path' => 'Default\Category\Economics.PNG'],
-            ['name' => 'Finance', 'public' => true, 'categories_id' => 1, 'image_path' => 'Default\Category\Finance.PNG'],
-            ['name' => 'Management', 'public' => true, 'categories_id' => 1, 'image_path' => 'Default\Category\Management.PNG'],
-            ['name' => 'Marketing', 'public' => true, 'categories_id' => 1, 'image_path' => 'Default\Category\Marketing.PNG'],
-        ];
-
-        foreach ($skillData as $data) {
-            $skill = Skill::create([
-                'name' => $data['name'],
-                'public' => $data['public'],
-                'categories_id' => $data['categories_id'],
-            ]);
-
-            $imagePath = asset($data['image_path']);
-            $skill->addMediaFromUrl($imagePath)->toMediaCollection('image_catogory');
-        }
     }
 
+    
     /**
      * Reverse the migrations.
      */
