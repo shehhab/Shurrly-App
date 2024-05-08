6737c7eda36d16a5072d345d26277cb6b7837751
shehhab <shehabahsraf308@gmail.com>
2024-05-08 13:43:25 +0300

    update path

--- app/Console/Commands/Initialization/InitRolesCommand.php
+++ app/Console/Commands/Initialization/InitRolesCommand.php
@@ -48,10 +48,67 @@ class InitRolesCommand extends Command
             'categories_id' => 1,
         ]);
 
-        $imagePath = asset('Default/Category/Accounting.png');
+        $imagePath1 = asset('Default/Category/Accounting.png');
 
-        $skill1->addMediaFromUrl($imagePath)
-               ->toMediaCollection('image_catogory');
+        $skill1->addMediaFromUrl($imagePath1)->toMediaCollection('image_catogory');
+
+
+        $skill2 = Skill::create([
+                'name' => 'Business',
+                'public' => true,
+                'categories_id' => 1,
+            ]);
+
+        $imagePath2 = asset('Default/Category/Business.png');
+
+        $skill2->addMediaFromUrl($imagePath2)->toMediaCollection('image_catogory');
+
+
+
+
+        $skill3 = Skill::create([
+            'name' => 'Economics',
+            'public' => true,
+            'categories_id' => 1,
+        ]);
+
+    $imagePath3 = asset('Default/Category/Economics.png');
+
+    $skill3->addMediaFromUrl($imagePath3)->toMediaCollection('image_catogory');
+
+    $skill4 = Skill::create([
+        'name' => 'Finance',
+        'public' => true,
+        'categories_id' => 1,
+    ]);
+
+    $imagePath4 = asset('Default/Category/Finance.png');
+
+    $skill4->addMediaFromUrl($imagePath4)->toMediaCollection('image_catogory');
+
+
+
+    $skill5 = Skill::create([
+        'name' => 'Management',
+        'public' => true,
+        'categories_id' => 1,
+    ]);
+
+    $imagePath5 = asset('Default/Category/Management.png');
+
+    $skill5->addMediaFromUrl($imagePath5)->toMediaCollection('image_catogory');
+
+
+
+    $skill6 = Skill::create([
+        'name' => 'Marketing',
+        'public' => true,
+        'categories_id' => 1,
+    ]);
+
+    $imagePath6 = asset('Default/Category/Marketing.png');
+
+    $skill6->addMediaFromUrl($imagePath6)->toMediaCollection('image_catogory');
 
         $this->info('Roles and skills initialized successfully.');
 
