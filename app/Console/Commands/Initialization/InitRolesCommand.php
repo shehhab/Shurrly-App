84a320c763c2fe88ff74dc0b3e3a69af6a428c6e
shehhab <shehabahsraf308@gmail.com>
2024-05-15 10:38:24 +0300

    COMMIT

--- app/Console/Commands/Initialization/InitRolesCommand.php
+++ app/Console/Commands/Initialization/InitRolesCommand.php
@@ -328,7 +328,7 @@ class InitRolesCommand extends Command
         $userAdvisor4->skills()->attach($skill->id);
         $userAdvisor4->save();
 
-        $imagePathAdvisor4 = asset('Default/Category/7.JPG');
+        $imagePathAdvisor4 = asset('Default/Category/4.JPG');
         $userAdvisor->addMediaFromUrl($imagePathAdvisor4)->toMediaCollection('advisor_profile_image');
 
 
