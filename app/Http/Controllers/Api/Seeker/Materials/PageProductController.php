54375486f8e418f59d89f594345975beffca5bfe
shehhab <shehabahsraf308@gmail.com>
2024-05-26 15:34:51 +0300

    update page

--- app/Http/Controllers/Api/Seeker/Materials/PageProductController.php
+++ app/Http/Controllers/Api/Seeker/Materials/PageProductController.php
@@ -29,7 +29,9 @@ class PageProductController extends Controller
         $formattedProduct = [
             'product_id' => $product->id,
             'product_title' => $product->title,
+            'created_by' => $product->advisor->seeker->name,
             'product_price' => $product->price,
+            'created_at' => $product->created_at->format('j,Y'),
             'product_description' => $product->description,
             'product_cover_photo' => $product->getFirstMediaUrl('cover_product'),
             'average_rate' => number_format($averageRate, 1) ?? 0,
@@ -53,11 +55,31 @@ class PageProductController extends Controller
 
         if ($product->video_duration !== null) {
             $formattedProduct['video_duration'] = $product->video_duration;
-            $formattedProduct['type'] =  'video' ;
-            // ! this is remove when complete payment
-            $formattedProduct['video'] =$product->getFirstMediaUrl('Product_Video');
 
+            $durationInSeconds = strtotime("1970-01-01 " . $product->video_duration . " UTC");
+
+            // تحويل المدة إلى تنسيق مختصر
+            if ($durationInSeconds < 60) {
+                $formattedDuration = $durationInSeconds . " ".'Sec'; // ثواني
+            } elseif ($durationInSeconds < 3600) {
+                $minutes = gmdate("i", $durationInSeconds);
+                $seconds = gmdate("s", $durationInSeconds);
+                if ($seconds >= 30) {
+                    $minutes++; // زيادة الدقائق
+                    $seconds = 0; // إعادة الثواني إلى صفر
+                }
+                $formattedDuration = $minutes ." ". 'Min' ; // دقائق وثواني
+            } else {
+                $hours = gmdate("H", $durationInSeconds);
+                $minutes = gmdate("i", $durationInSeconds);
+                $formattedDuration = $hours . ':' . $minutes; // ساعات ودقائق
+            }
+
+            $formattedProduct['video_duration'] = $formattedDuration;
+            $formattedProduct['type'] = 'video';
+            $formattedProduct['video'] = $product->getFirstMediaUrl('Product_Video');
         }
+
 
         if ($product->pdf_page_count !== null) {
             $formattedProduct['pdf_page_count'] = $product->pdf_page_count;
